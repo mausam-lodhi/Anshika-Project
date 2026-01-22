33f8d983afbc8beede71e5f3f436e7ad2a92a9a2
mausam-lodhi <harprasadlodhi1984@gmail.com>
2026-01-22 21:16:13 +0530

    Added full Shiksha-Kendra MERN project

--- Shiksha-Kendra/mern-server/index.js
+++ Shiksha-Kendra/mern-server/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(
 	cors({
-		origin: "https://anshika-project.onrender.com",
+		origin: process.env.CLIENT_URL || "https://anshika-project.onrender.com",
 		methods: ["GET", "POST", "PUT", "DELETE"],
 		credentials: true,
 	})
